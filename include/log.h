3be8da157c9bfad281f19b1108711b664063790c
Michal Kukowski <michal.kukowski@nokia.com>
2017-02-17 13:51:40 +0100

    log interface

--- include/log.h
+++ include/log.h
@@ -10,13 +10,22 @@
 	LICENCE: GPL3
 */
 
+#include <stdio.h>
+
 /*
 	Modes to debug:
-	DEBUG_MODE -> if defined, log and error works
+	DEBUG_MODE -> if defined, log works
 
 	TRACE_MODE -> if defined trace_call works
 
-    To avoid no-print after bug, we log everything
+    SILENT_ERROR -> if defined error logged errors
+
+    Usage:
+    1. First init logs: log_init
+        IF you want to store logs in file pass LOG_TO_FILE to function
+    2. Normal usage: TRACE, LOG, ERROR in your functions
+    3. Clean log system: log_deinit
+
 */
 
 #define __TRACE__   "[TRACE]\tFUNC: %s\n", __func__
@@ -28,7 +37,6 @@
         __trace_call__(__TRACE__, ##__VA_ARGS__); \
     }while(0)
 
-
 #define LOG(msg, ...) \
     do{ \
         __log__(__LOG__); \
@@ -38,16 +46,19 @@
 
 #define ERROR(msg, errno, ...) \
     do{ \
-        __log__(__ERROR__); \
-        __log__("\t"); \
-        __log__(msg, ##__VA_ARGS__); \
+        __error__(__ERROR__); \
+        __error__("\t"); \
+        __error__(msg, ##__VA_ARGS__); \
         \
         return errno; \
     }while(0)
-/*
-	Simple log function, put msg to stderr
-	FUNCTION WORKS IFF DEBUG_MODE is defined
 
+#define NO_LOG_TO_FILE  0
+#define LOG_TO_FILE     1
+
+/*
+	Simple log function
+	FUNCTION WORKS IFF DEBUG_MODE is defined
 
 	PARAMS
 	@IN msg - log message
@@ -69,5 +80,41 @@ void __log__(const char *msg, ...);
 	This is a void function
 */
 void __trace_call__(const char *msg, ...);
+
+/*
+	Simple log error function
+	FUNCTION WORKS IFF SILENT_ERROR is not defined
+
+	PARAMS
+	@IN msg - log message
+
+	RETURN:
+	This is a void function
+*/
+void __error__(const char *msg, ...);
+
+/*
+    initialize log system
+
+    PARAMS
+    @IN fd - FILE*, if NULL use default stderr
+    @IN to_file log to special file ? LOG_TO_FILE - yes, NO_LOG_TO_FILE no
+
+    RETURN
+    0 iff success
+    non-zero value iff failure
+*/
+int log_init(FILE *fd, int to_file);
+
+/*
+    deinit log system
+
+    PARAMS
+    NO PARAMS
+
+    RETURN
+    This is a void function
+*/
+void log_deinit(void);
 
 #endif
--- src/log.c
+++ src/log.c
@@ -1,31 +1,153 @@
 #include <log.h>
 #include <stdio.h>
 #include <stdarg.h>
+#include <sys/stat.h> 	/* stat */
+#include <time.h> 		/* strftime */
+#include <string.h> 	/* strlen, SSE  */
+#include <fcntl.h>
+
+static FILE *logfd = NULL;
+static FILE *logfile = NULL;
+static const char const *logdir = "./logs";
+
+#define LOGBUF_SIZE	128
 
 #ifdef DEBUG_MODE
-
 void __log__(const char *msg, ...)
 {
 	va_list args;
 
-    va_start (args, msg);
-	vfprintf(stderr, msg, args);
+    va_start(args, msg);
+	(void)vfprintf(logfd, msg, args);
 	va_end(args);
-}
 
+	if (logfile != NULL)
+	{
+		va_start (args, msg);
+		(void)vfprintf(logfile, msg, args);
+		va_end(args);
+	}
+}
+#else
+void __log__(const char *msg, ...) { }
+#endif
+
+#ifdef TRACE_MODE
 void __trace_call__(const char *msg, ...)
 {
     va_list args;
 
-    va_start (args, msg);
-	vfprintf(stderr, msg, args);
+    va_start(args, msg);
+	(void)vfprintf(logfd, msg, args);
 	va_end(args);
+
+	if (logfile != NULL)
+	{
+		va_start (args, msg);
+		(void)vfprintf(logfile, msg, args);
+		va_end(args);
+	}
+}
+#else
+void __trace_call__(const char *msg, ...) { }
+#endif
+
+#ifndef SILENT_ERROR
+void __error__(const char *msg, ...)
+{
+	va_list args;
+
+    va_start(args, msg);
+	(void)vfprintf(logfd, msg, args);
+	va_end(args);
+
+	if (logfile != NULL)
+	{
+		va_start (args, msg);
+		(void)vfprintf(logfile, msg, args);
+		va_end(args);
+	}
+}
+#else
+void __error__(const char *msg, ...) { }
+#endif
+
+int log_init(FILE *fd, int to_file)
+{
+	struct stat st;
+	struct tm t;
+	time_t now;
+
+	char buf[LOGBUF_SIZE];
+	size_t len;
+
+	if (fd == NULL || fd != stderr || fd != stdout)
+		logfd = stderr;
+	else
+		logfd = fd;
+
+	if (to_file == LOG_TO_FILE)
+	{
+		/* create directory if doesn't exist */
+		if (stat(logdir, &st) == -1)
+			if (mkdir(logdir, 0775) == -1)
+			{
+				perror("mkdir error\n");
+				return 1;
+			}
+
+		len = strlen(logdir);
+		if (memcpy((void*)buf, (void*)logdir, len) == NULL)
+		{
+			perror("memcpy error\n");
+			return 1;
+		}
+
+		/* get local time and create string - file name */
+		now = time(NULL);
+		t = *localtime(&now);
+		if (strftime(buf + len, LOGBUF_SIZE, "/%Y%m%d%H%M%S.log", &t) == 0)
+		{
+			perror("strftime error\n");
+			return 1;
+		}
+
+		/* check existing of file */
+		if (stat(buf, &st) == -1)
+		{
+			if (creat(buf, 0644) == -1)
+			{
+				perror("creat error\n");
+				return 1;
+			}
+		}
+		else
+		{
+		 	perror("Logs file exists\n");
+			return 1;
+		}
+
+		/* open */
+		logfile = fopen(buf, "w");
+		if (logfile == NULL)
+		{
+			perror("fopen error\n");
+			return 1;
+		}
+	}
+	else
+		logfile = NULL;
+
+	return 0;
 }
 
-#else
+void log_deinit(void)
+{
+	if (logfile != NULL)
+	{
+		(void)fclose(logfile);
+		logfile = NULL;
+	}
 
-/* empty function, because we have not enable debugs */
-void __log__(const char *msg, ...) { }
-void __trace_call__(const char *msg, ...) { }
-
-#endif
+	logfd = NULL;
+}
