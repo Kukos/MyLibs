73aef8862d31cc9973d7a32fdb08d26a66a68cee
Michal Kukowski <michal.kukowski@nokia.com>
2017-03-27 13:46:25 +0200

    test framework

---
+++ include/test.h
@@ -0,0 +1,79 @@
+#ifndef TEST_H
+#define TEST_H
+
+/*
+    Simple test framework
+
+    Author: Michal Kukowski
+    email: michalkukowski10@gmail.com
+
+    LICENCE: GPL3.0
+*/
+
+#include <common.h>
+#include <stdio.h>
+#include <compiler.h>
+
+/* PRIVATE MACRO */
+#define ____MOCK_F____(func) concat(___mock___, func)
+
+/*
+    Use this macro to call your test func,
+
+    example:
+    int test1(void)
+    {
+        ...
+        return ret;
+    }
+
+    TEST(test1());
+
+*/
+#define TEST(func) \
+    do { \
+        if (func) \
+            printf("[TEST]\t%s\t" RED "FAILED" RESET "\n", #func); \
+        else \
+            printf("[TEST]\t%s\t" GREEN "PASSED" RESET "\n", #func); \
+    } while(0);
+
+
+/*
+    Use this macro to call funtion @func and cmp ret value with @val
+*/
+#define EXPECT(func, val) \
+    __extension__ ({ \
+            (func != val) ? \
+            (CAST_TO_BOOL(printf("[TEXPECT]\t%s return bad value\n", #func))) : 0; \
+    })
+
+/*
+    Use this macro to compare val and expected val
+*/
+#define ASSERT(val, expt) \
+    __extension__ ({ \
+        (val != expt) ? \
+        (CAST_TO_BOOL(printf("[TASSERT]\t%s bad value\n", __func__))) : 0; \
+    })
+
+/*
+    Use this macro to mock funtion
+*/
+#define MOCK(func, val) \
+    typeof(val) ____MOCK_F____(func)(__unused__ int guard, ...) { return val; }
+
+/*
+    Use this macro to mock void funtion
+*/
+#define MOCK_V(func) \
+    void ____MOCK_F____(func)(__unused__ int guard, ...) { }
+
+/*
+    Use this macro to call mock fuction
+*/
+#define CALL(func, ...) \
+    ____MOCK_F____(func)(0, ##__VA_ARGS__)
+
+
+#endif
