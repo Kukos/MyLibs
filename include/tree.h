b428840817154dd3900bbbd21dd3dac7b9790085
Michal Kukowski <michal.kukowski@nokia.com>
2017-04-03 11:53:06 +0200

    Description in tree.h

--- include/tree.h
+++ include/tree.h
@@ -4,6 +4,10 @@
 /*
     Tree Container Framework
 
+    To use this framework, plase provide constructor function for your tree
+    i. e. tree_bst_create( ... )
+    and in function assign pointers to functions
+
     Author: Michal Kukowski
     email: michalkukowski10@gmail.com
 
@@ -32,55 +36,179 @@ typedef struct Tree
     int         ____height(void *tree);
 }Tree;
 
+/*
+    Destroy Tree
+
+    PARAMS
+    @IN tree - pointer to Tree
+
+    RETURN:
+    This is a void function
+*/
 __inline__ void tree_destroy(Tree *tree)
 {
     tree->____destroy(tree->tree);
 }
 
+
+/*
+    Insert new data to Tree
+
+    PARAMS
+    @IN tree - pointer to Tree
+    @IN data - address of data
+
+    RETURN:
+    0 iff success
+    Non-zero value iff failed
+*/
 __inline__ int tree_insert(Tree *tree, void *data)
 {
     return tree->____insert(tree->tree, data);
 }
 
+/*
+    Delet data to Tree
+
+    PARAMS
+    @IN tree - pointer to Tree
+    @IN data - address of data
+
+    RETURN:
+    0 iff success
+    Non-zero value iff failed
+*/
 __inline__ int tree_delete(Tree *tree, void *data)
 {
     return tree->____delete(tree->tree, data);
 }
 
+/*
+    Get data with min key
+
+    PARAMS
+    @IN tree - pointer to Tree
+    @IN data - address of min data
+
+    RETURN:
+    0 iff success
+    Non-zero value iff failed
+*/
 __inline__ int tree_min(Tree *tree, void *data)
 {
     return tree->____min(tree->tree, data);
 }
+
+/*
+    Get data with max key
+
+    PARAMS
+    @IN tree - pointer to Tree
+    @IN data - address of max data
+
+    RETURN:
+    0 iff success
+    Non-zero value iff failed
+*/
 __inline__ int tree_max(Tree *tree, void *data)
 {
     return tree->____max(tree->tree, data);
 }
 
+
+/*
+    Get data with key equal to fake @data
+
+    PARAMS
+    @IN tree - pointer to Tree
+    @IN data - address of fake data with correct key
+    @IN data_out - address of data
+
+    RETURN:
+    0 iff success
+    Non-zero value iff failed
+*/
 __inline__ int tree_search(Tree *tree, void *data_in, void *data_out)
 {
     return tree->____search(tree->tree, data_in, data_out);
 }
 
+/*
+    Check existing of data with key in @data
+
+    PARAMS
+    @IN tree - pointer to Tree
+    @IN data - address of fake data with correct key
+
+    RETURN:
+    true - iff data exist
+    false iff data not exists
+*/
 __inline__ bool tree_key_exist(Tree *tree, void *data)
 {
     return tree->____key_exist(tree->tree, data);
 }
 
+
+/*
+    Balance Tree ( iff not balanced )
+
+    PARAMS
+    @IN tree - pointer to Tree
+
+    RETURN:
+    0 iff success
+    Non-zero value iff failed
+*/
 __inline__ int tree_balance(Tree *tree)
 {
     return tree->____balance(tree->tree);
 }
 
+/*
+    Convert Tree to Array
+
+    PARAMS
+    @IN tree - pointer to Tree
+    @IN array - address of array
+    @IN size - array size
+
+    RETURN:
+    0 iff success
+    Non-zero value iff failed
+*/
 __inline__ int tree_to_array(Tree *tree, void **array, size_t *size)
 {
     return tree->____toarray(tree->tree, array, size);
 }
 
+/*
+    Get Num of entries in Tree
+
+    PARAMS
+    @IN tree - pointer to tree
+
+    RETURN:
+    0 iff tree is empty or fail
+    %Num of Entries iff success
+
+*/
 __inline__ size_t tree_num_enries(Tree *tree)
 {
     return tree->____num_entries(tree->tree);
 }
 
+/*
+    Get Tree Height
+
+    PARAMS
+    @IN tree
+
+    RETURN:
+    -1 iff failure
+    %Height
+
+*/
 __inline__ int tree_height(Tree *tree)
 {
     return tree->____height(tree->tree);
