3b03671f67b17d6a45f03fe27cc753eae467a322
Michal Kukowski <michalkukowski10@gmail.com>
2018-12-26 23:39:42 +0100

    Fix mem leak in tls for ld ssl versions

--- src/tls/tls.c
+++ src/tls/tls.c
@@ -144,6 +144,12 @@ void tls_clenup(void)
     CRYPTO_set_locking_callback(NULL);
     CRYPTO_set_id_callback(NULL);
     ENGINE_cleanup();
+#if OPENSSL_VERSION_NUMBER < 0x10000000L
+	ERR_remove_state(0);
+#endif
+#if OPENSSL_VERSION_NUMBER < 0x10100000L
+	ERR_remove_thread_state(NULL);
+#endif
     CONF_modules_free();
     CONF_modules_unload(1);
     COMP_zlib_cleanup();
